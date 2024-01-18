87a80fc9a5f462e464f55f1e30cf9174d06c2009
Tri Tran <thuutri2710@gmail.com>
2024-01-18 18:08:06 +0700

    feat: add disallow-no-translation-text rule

--- configs/all.js
+++ configs/all.js
@@ -3,29 +3,16 @@
 const fromEntries = require("object.fromentries");
 const entries = require("object.entries");
 
-const allRules = require("../lib/rules");
-
-function filterRules(rules, predicate) {
-  return fromEntries(entries(rules).filter((entry) => predicate(entry[1])));
-}
-
-function configureAsError(rules) {
-  return fromEntries(Object.keys(rules).map((key) => [`react/${key}`, 2]));
-}
-
-const activeRules = filterRules(allRules, (rule) => !rule.meta.deprecated);
-const activeRulesConfig = configureAsError(activeRules);
-
-const deprecatedRules = filterRules(allRules, (rule) => rule.meta.deprecated);
+const rule = require("../lib/rules/disallow-no-translation-text");
 
 module.exports = {
   plugins: {
     react: {
       deprecatedRules,
-      rules: allRules,
+      rules: [rule],
     },
   },
-  rules: activeRulesConfig,
+  rules: [rule],
   languageOptions: {
     parserOptions: {
       ecmaFeatures: {
