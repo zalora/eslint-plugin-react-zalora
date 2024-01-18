76841eb9f200413384180d3f6e87cd28b8cfcbd4
Tri Tran <thuutri2710@gmail.com>
2024-01-18 18:49:49 +0700

    feat: add disallow-no-translation-text rule

--- index.js
+++ index.js
@@ -3,22 +3,13 @@
 const configAll = require("./configs/all");
 const configRecommended = require("./configs/recommended");
 
-const rule = require("./lib/rules/disallow-no-translation-text");
-
 // for legacy config system
 const plugins = ["react-zalora"];
 
 module.exports = {
-  rules: {
-    "disallow-no-translation-text": rule,
-  },
   configs: {
     recommended: Object.assign({}, configRecommended, {
       parserOptions: configRecommended.languageOptions.parserOptions,
-      plugins,
-    }),
-    all: Object.assign({}, configAll, {
-      parserOptions: configAll.languageOptions.parserOptions,
       plugins,
     }),
   },
