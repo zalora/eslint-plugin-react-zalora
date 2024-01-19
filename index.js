294cbaea3db5a8a1b95d998db596bd443a908c9a
Tri Tran <thuutri2710@gmail.com>
2024-01-19 08:44:19 +0700

    chore: add log

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const configAll = require("./configs/all");
+const configRecommended = require("./configs/recommended");
+
+// for legacy config system
+const plugins = ["react-zalora"];
+
+const rule = require("./lib/rules/disallow-no-translation-text");
+
+module.exports = {
+  configs: {
+    recommended: Object.assign({}, configRecommended, {
+      parserOptions: configRecommended.languageOptions.parserOptions,
+      plugins,
+    }),
+  },
+  rules: {
+    "disallow-no-translation-text": rule,
+  },
+};
